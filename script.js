3602c7733e35313378252d81430336fc2abe5227
bokigolic <bokigolic32@gmail.com>
2023-02-16 09:58:41 -0500

    add navbar

---
+++ script.js
@@ -0,0 +1,6 @@
+/*var imageClick = document.getElementById("image-click");
+var formContainer = document.querySelector(".form-container");
+
+imageClick.addEventListener("click", function() {
+  formContainer.classList.toggle("open");
+})*/
